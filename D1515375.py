893f1e76e23a45e7570d31230fc501be606735ef
鍾允碩 <D1515375@o365.fcu.edu.tw>
2026-09-17 11:22:38 +0800

    123

--- D1515375.py
+++ D1515375.py
@@ -1,13 +1,8 @@
-print('*')
-print('**')
-print('***')
-print('****')
-print('*****')
-print('*****')
-print('*****')
-print('*****')
-print('*****')
-print('****')
-print('***')
-print('**')
-print('*')
+for i in range(1, 5):
+    print("*" * i)
+
+for i in range(5):
+    print("*****")
+
+for i in range(4, 0, -1):
+    print("*" * i)
